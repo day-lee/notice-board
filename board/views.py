97bd641e99cc48539aeddd25f6fec2cdcd7ebe5d
Dayeon Lee <iamdayeonlee@gmail.com>
2021-09-27 16:17:30 +0900

    Add filter

--- board/views.py
+++ board/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView
                                   )
 from .models import Post
+from .filters import PostFilter
 from .forms import PostForm, EditForm
 
 class PostListView(ListView):
@@ -10,6 +11,18 @@ class PostListView(ListView):
     # context_object_name = 'posts'
     ordering = ['-post_created']
     paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filter = PostFilter(self.request.GET, queryset)
+        return filter.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        queryset = self.get_queryset()
+        filter = PostFilter(self.request.GET, queryset)
+        context["filter"] = filter
+        return context
 
 
 class PostDetailView(DetailView):
@@ -35,7 +48,8 @@ class PostUpdateView(UpdateView):
     model = Post
     fields = '__all__'
     template_name = 'notice_board/post_form_django.html'
-
+    #template_name = 'notice_board/post_form.html'
+    # 빈 폼이 출력됌, post_form_django.html의 form.html의 html 요소가 post_form과 동일해서 작동은 됐음.
 
 class PostDeleteView(DeleteView):
     model = Post
